e12319abcf1fa483a8faac0af86181971432e7c1
M-Fajar <m.fajar980527@gmail.com>
2022-02-21 21:18:04 +0700

    Change Config Prod

--- config/config.js
+++ config/config.js
@@ -16,11 +16,6 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    ssl: true,
+    use_env_variable: process.env.DATABASE_URL,
   },
 };
